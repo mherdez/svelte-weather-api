51042475f4e5c3325aba53c22022040c67791e77
Manuel Herdez <herdez@gmail.com>
2023-05-07 17:55:48 -0600

    build docs

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      output: {
+        dir: 'docs'
+      }
+    }
+  }
 })
